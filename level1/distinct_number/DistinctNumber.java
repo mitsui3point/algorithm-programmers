3509d669b96ba93a84ab324179625a1cfe655921
mitsui3point <rladbsgh0331@gmail.com>
2023-08-29 22:05:22 +0900

    feat: add distinct_number(같은 숫자는 싫어)

---
+++ level1/distinct_number/DistinctNumber.java
@@ -0,0 +1,28 @@
+package level1.distinct_number;
+
+import java.util.*;
+
+/**
+ * https://school.programmers.co.kr/learn/courses/30/lessons/12906
+ */
+public class DistinctNumber {
+    public static void main(String[] args) {
+        boolean isTrue = solution(new int[]{1, 1, 3, 3, 0, 1, 1}) == new int[]{1, 3, 0, 1};
+        System.out.println("isTrue = " + isTrue);
+    }
+    public static int[] solution(int[] arr) {
+        Stack<Integer> stack = new Stack();
+        List<Integer> result = new ArrayList();
+        stack.push(arr[0]);
+        for (Integer num : arr) {
+            if (stack.pop() == num) {
+                stack.push(num);
+                continue;
+            }
+            stack.push(num);
+            result.add(num);
+        }
+        System.out.println(stack);
+        return result.stream().mapToInt(i -> i).toArray();
+    }
+}
