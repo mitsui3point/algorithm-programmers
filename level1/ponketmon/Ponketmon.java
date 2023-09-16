e4979f6631d35f2aac635eb86c9f5341f5ebb257
mitsui3point <rladbsgh0331@gmail.com>
2023-09-16 18:07:09 +0900

    feat: add ponketmon(폰켓몬; hash)

---
+++ level1/ponketmon/Ponketmon.java
@@ -0,0 +1,24 @@
+package level1.ponketmon;
+
+import java.util.*;
+
+/**
+ * hash
+ * https://school.programmers.co.kr/learn/courses/30/lessons/1845
+ */
+public class Ponketmon {
+    public static void main(String[] args) {
+        boolean isTrue = solution(new int[]{3, 3, 3, 2, 2, 4}) == 3;
+        System.out.println("isTrue = " + isTrue);
+    }
+
+    private static int solution(int[] nums) {
+        int answer = 0;
+        Set<Integer> set = new HashSet();
+        for (int num : nums) {
+            set.add(num);
+        }
+        return set.size() < nums.length / 2 ? set.size() : nums.length / 2;
+    }
+
+}
